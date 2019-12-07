77436320499a4688bdaf477c1bad34baab6dbb04
ArekLopus <9700614+ArekLopus@users.noreply.github.com>
2019-12-07 10:19:52 +0100

    Update CorsBuilder.java
    description fix

--- src/main/java/cors/jaxrs/CorsBuilder.java
+++ src/main/java/cors/jaxrs/CorsBuilder.java
@@ -2,23 +2,23 @@ package cors.jaxrs;
 
 import cors.enums.CorsType;
 
+/*
+*    Copyright 2019 Arkadiusz Lopuszynski 
+* 
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+* 
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 /**
- * 
- *    Copyright 2019 Arkadiusz Lopuszynski 
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * 
- * 
  * CORS information: 
  * <a target="_blank" rel="noopener noreferrer" href="https://www.w3.org/TR/cors/">W3C</a>, 
  * <a target="_blank" rel="noopener noreferrer" href="https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS">MDN</a>, 
